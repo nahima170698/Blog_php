9d6a9c201ee2dd9e3d6fe49e02e115b749941e24
Nahima <nahima17.l@gmail.com>
2023-06-16 09:03:29 +0400

    Cookery

---
+++ Cookery/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>Voici mon footer</p>
+</footer>
---
+++ Cookery/header.php
@@ -0,0 +1,14 @@
+<header>
+        <nav id="NavigationBar">
+            <div id="HamburgerMenu">
+                <!-- We are swapping between the Hamburger button and the UnorderedList responsively -->
+                <button id="HamburgerButton" class="navigational"><img src="./cache/icons_Hamburger.png" onclick="ToggleHamburger()"></button>
+                <ul id="HamLinks" class="Collapsed">   
+                    <li class="navigational selected"><a href="./index.html">Accueil</a></li>
+                    <li class="navigational"><a href="./articles.html">Articles</a></li>
+                    <li class="navigational"><a href="./contact.html">Contact</a></li>
+                </ul>
+            </div>
+            <a id="Login" class="navigational" href="./login.html">Login</a>
+        </nav>
+    </header>
